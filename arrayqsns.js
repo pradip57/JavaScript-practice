f47994b94027f75ee45bb92e22771798a6f72965
Pradip <dellpidarp57@gmail.com>
2024-05-01 13:50:00 +0545

    map method complted with qsns

--- arrayqsns.js
+++ arrayqsns.js
@@ -10,16 +10,23 @@ console.log(fFruits);
 
 //using map method write a function that takes an array of numbers and return sa new array where each number is squared but only if its an even number
 
-const numbs = [3,2,4,1,7,8,9]
+const numbs = [3, 2, 4, 1, 7, 8, 9];
 
-const finalNums = numbs.map((currentElem)=>{
+const finalNums = numbs.map((currentElem) => {
+  if (currentElem % 2 == 0) {
+    currentElem = currentElem * currentElem;
+  }
+  return currentElem;
+});
 
-    if(currentElem % 2 == 0){
-         currentElem = currentElem * currentElem
-    }
+console.log(finalNums);
 
-    return currentElem
-})
+//using the map method write a functions that takes the array of names and retuns a new array where each name is prefix with Mr.
 
+const users = ["Pradip", "Ram", "Shayam", "Hari"];
 
-console.log(finalNums)
+const result = users.map((currElem) => {
+  return (currElem = "Mr." + currElem);
+});
+
+console.log(result);
