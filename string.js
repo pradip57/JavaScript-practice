b4d8523d03b9c52050d9e0466c58114308d7c70d
Pradip <dellpidarp57@gmail.com>
2024-05-02 13:55:00 +0545

    string in js

---
+++ string.js
@@ -0,0 +1,17 @@
+//string created with single or double quote
+//length in string
+
+let a = "Pradip Saapkota"
+console.log(a.length)
+
+//created string using backtick
+
+let b  = `Hello "my" name is pradip
+sapkota. i am a student`
+
+b = b.toUpperCase()
+
+
+console.log(b)
+
+
