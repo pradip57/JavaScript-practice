77918b9939a4464c40510ef64154b3573c048d45
Pradip <dellpidarp57@gmail.com>
2024-05-02 14:24:50 +0545

    conversion of strin g to array

--- string.js
+++ string.js
@@ -1,17 +1,36 @@
 //string created with single or double quote
 //length in string
 
-let a = "Pradip Saapkota"
-console.log(a.length)
+let a = "Pradip Saapkota";
+console.log(a.length);
 
 //created string using backtick
 
-let b  = `Hello "my" name is pradip
-sapkota. i am a student`
+let b = `Hello "my" name is pradip
+sapkota. i am a student`;
 
-b = b.toUpperCase()
+b = b.toUpperCase();
+
+console.log(b);
+
+//indexOf: return first occurence of char if found //case sensative
+
+//return -1 if char doesnot found
+
+let c = b.indexOf("AM");
+
+console.log(c);
+
+//conversion of string into array
+
+let d = Array.from(b);
+console.log(d)
 
 
-console.log(b)
+let afterMap = d.map((curV,index)=>{
 
+    return `${curV} - ${index}`
 
+})
+
+console.log(afterMap)
