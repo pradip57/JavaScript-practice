7558b6acbb7b06a44cfdc98d89427269797dc47c
Pradip <dellpidarp57@gmail.com>
2024-05-05 14:15:02 +0545

    includes in js

--- match.js
+++ match.js
@@ -1,16 +1,22 @@
 //match and matchAll method is oly for string
 
-const a = [5,4,6,7,8,9,5,3,4,3]
+const a = [5, 4, 6, 7, 8, 9, 5, 3, 4, 3];
 
+let b = "";
 
-let b = ""
-
-for(let  i=0;i<a.length;i++){
-
-    b = b + a[i]
+for (let i = 0; i < a.length; i++) {
+  b = b + a[i];
 }
 
-console.log(b)
+console.log(b);
 
-let d = b.match(/5/g) //checks how many times it appears
-console.log(d)
+let d = b.match(/5/g); //checks how many times it appears
+console.log(d);
+
+//includes
+
+
+let str1 = "My name is pradip sapkota"
+
+console.log(str1.includes('prakt'))
+console.log(str1.includes('pradip'))
