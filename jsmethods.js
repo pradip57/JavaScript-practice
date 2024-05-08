bb9fabd5e423645f2a871f26173a9db989911152
Pradip <dellpidarp57@gmail.com>
2024-05-08 14:00:47 +0545

    str to array, array to string using split and join

--- jsmethods.js
+++ jsmethods.js
@@ -1,10 +1,19 @@
-const str = "my name is pradip"
+const str = "my name is pradip";
 
 //convert to upper case
-console.log(str.toUpperCase())
+console.log(str.toUpperCase());
 
 //removing white space from both ends
 
-const str1 = "  Hello  "
+const str1 = "  Hello  ";
 
-console.log(str1.trim())
+console.log(str1.trim());
+
+const fruit1 = "apple";
+const fruitInArray = fruit1.split("");
+
+console.log(fruitInArray);
+fruitInArray.push("1");
+console.log(fruitInArray);
+const fruitInString = fruitInArray.join("");
+console.log(fruitInString);
