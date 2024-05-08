5ae73f01b5dd654561d8b6316aa424afb6d94bc8
Pradip <dellpidarp57@gmail.com>
2024-05-08 13:56:14 +0545

    learning js builtin methods

---
+++ jsmethods.js
@@ -0,0 +1,10 @@
+const str = "my name is pradip"
+
+//convert to upper case
+console.log(str.toUpperCase())
+
+//removing white space from both ends
+
+const str1 = "  Hello  "
+
+console.log(str1.trim())
