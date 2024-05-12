ea3ca0c6f3c17bfd471a6155c05be055320491e4
Pradip <dellpidarp57@gmail.com>
2024-05-12 13:25:00 +0545

    math object practice

--- mathObject.js
+++ mathObject.js
@@ -6,11 +6,34 @@ console.log(Math.round(4.1));
 //math.floor
 
 console.log(Math.floor(4.9));
+console.log(Math.floor(-4.9));
 console.log(Math.floor(4.1));
 
 //math.ceil
 
 console.log(Math.ceil(4.9));
 console.log(Math.ceil(4.1));
+console.log(Math.ceil(-4.1));
+
+//constant
+
+console.log(Math.PI.toPrecision(3));
+
+//absolute
+
+console.log(Math.abs(-10));
+
+//trunc=> returns only integer part
+
+console.log(Math.trunc(33.7))
+
+//power
+
+console.log(Math.pow(2,3))
+console.log(2**3) //same
+
+//square root
+
+console.log(Math.sqrt(16))
 
 
