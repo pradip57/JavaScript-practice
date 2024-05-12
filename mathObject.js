ff3400956c377633f8adb49b73425e382e85fb16
Pradip <dellpidarp57@gmail.com>
2024-05-12 13:01:24 +0545

    math.object

---
+++ mathObject.js
@@ -0,0 +1,16 @@
+//math.round()
+
+console.log(Math.round(4.5));
+console.log(Math.round(4.1));
+
+//math.floor
+
+console.log(Math.floor(4.9));
+console.log(Math.floor(4.1));
+
+//math.ceil
+
+console.log(Math.ceil(4.9));
+console.log(Math.ceil(4.1));
+
+
